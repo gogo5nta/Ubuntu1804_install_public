0764083952ff6ea880ca4aced7f5a5d4adb2f592
Tomohide Ishigami <gogo5nta@gmail.com>
2021-04-24 11:28:55 +0900

    Update 00_tool_install.sh

--- install2/00_tool_install.sh
+++ install2/00_tool_install.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
-# update 2020.09.12
+# update 2021.04.24
 
 # rename English holder
 LANG=C xdg-user-dirs-gtk-update
 
 # install chromium-browser
-sudo apt-get install chromium-browser
+sudo apt-get install -y chromium-browser
 
 # install terminator
-sudo apt-get install terminator
+sudo apt-get install -y terminator
 
 # ssh
-sudo apt-get install openssh-server
+sudo apt-get install -y openssh-server
 
 # good tool gnome-tweaks
-sudo apt-get install gnome-tweaks
+sudo apt-get install -y gnome-tweaks
