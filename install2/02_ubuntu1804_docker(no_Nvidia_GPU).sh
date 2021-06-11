d7049b4493a7fed624f750572f48d1663e2c081a
Tomohide Ishigami <gogo5nta@gmail.com>
2021-06-11 14:21:14 +0900

    Update 02_ubuntu1804_docker(no_Nvidia_GPU).sh

--- install2/02_ubuntu1804_docker(no_Nvidia_GPU).sh
+++ install2/02_ubuntu1804_docker(no_Nvidia_GPU).sh
@@ -48,3 +48,6 @@ docker run hello-world
 # https://docs.docker.jp/compose/install.html#linux
 sudo curl -L https://github.com/docker/compose/releases/download/1.16.1/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
+
+# docker-composeの確認
+docker-compose --version
