6dd552202bdd2867efa3d4ee366fb0d064b4195b
Tomohide Ishigami <gogo5nta@gmail.com>
2021-06-11 14:02:13 +0900

    Update 02_ubuntu1804_docker_nvidia_container_tool_install.sh

--- install1/02_ubuntu1804_docker_nvidia_container_tool_install.sh
+++ install1/02_ubuntu1804_docker_nvidia_container_tool_install.sh
@@ -71,3 +71,8 @@ sudo systemctl restart docker
 
 # test
 docker run hello-world 
+
+# docker-composeのインストール
+# https://docs.docker.jp/compose/install.html#linux
+sudo curl -L https://github.com/docker/compose/releases/download/1.16.1/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
